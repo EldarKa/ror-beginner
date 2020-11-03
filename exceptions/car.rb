72648f9bb47bb13d1bc40a34c6629ada2b1fbdba
EldarKa <47607728+EldarKa@users.noreply.github.com>
2020-11-03 18:45:37 +0300

    поезда, станции, маршруты
    проба пера

--- exceptions/car.rb
+++ exceptions/car.rb
@@ -1,48 +1,90 @@
-class Car
-  attr_reader :current_rpm
-  attr_accessor :number
+  
+классная станция
+  def initialize(name_station)
+    @name_station = name_station
+    @stationTrains = массив.новый
+  конец
 
-  NUMBER_FORMAT = /^[а-я]{1}\d{3}[а-я]{2}$/i
+  def staying_train(name_train) # поезд прибыл на станцию
+    
+    @stationTrains = @stationTrains + [name_train]
+    @stationTrains.каждый {/поезд/ ставит поезд }
+  конец
 
-  def initialize(number)    
-    @current_rpm = 0
-    @number = number
-    validate!
-  end
+  def waning_train(name_train) # поезд покидает станцию
+    
+    @stationTrains = @stationTrains - [name_train]
+    @stationTrains.каждый {/поезд/ ставит поезд }
+  конец
 
-  def start_engine
-    start_engine! if engine_stopped?
-  end
+конец
 
-  def engine_stopped?
-    current_rpm.zero?
-  end
-
-  def valid?
-    validate!
-  rescue
-    false
-  end
-
-  protected
-
-  attr_writer :current_rpm
-
-  def validate!
-    raise "Number can't be nil" if number.nil?
-    raise "Number should be at least 6 symbols" if number.length < 6
-    raise "Number has invalid format" if number !~ NUMBER_FORMAT
-    true
-  end
-
-  def initial_rpm
-    700
-  end
-
-  def start_engine!
-    self.current_rpm = initial_rpm
-  end
-
-  # остановить двигатель
-end
-
+классный маршрут
+  attr_reader :@route0 # выводим всесь маршрут для прикрепления к поезду
+  def initialize(start_station, finish_station) # начало и конец маршрута
+    @start_station = [start_station]
+    @finish_station = [finish_station]
+    @route0 = @start_station + @finish_station
+    @intermediate_station = массив.новый
+  конец
+  
+  def intermediate_station(name_station) # добавление станции
+    @intermediate_station= @intermediate_station + [name_station]
+    @route0 = @start_station + @intermediate_station + @finish_station
+    
+  конец
+  
+  def delete_station(name_station) # удаление станции
+    @intermediate_station = @intermediate_station - [name_station]
+    @route0 = @start_station + @intermediate_station + @finish_station
+    
+  конец
+  
+   вывод def
+    @маршрут.each { |x| puts x } # перечисление маршрута
+  конец
+конец
+классный поезд
+  def initialize(number,type1,wagon) # номер тип транспорта число вагонов
+    @number = число
+    @type1 = type1
+    @wagon = вагон
+    @скорость=0
+  конец
+  
+  
+  def speed_change(num) #меняем скорость на любое значение, при отрицательном значение скорость 0
+    @speed=@speed+num
+    если @speed < 0
+      @скорость=0
+      put @speed
+    конец
+  конец
+  def wagon_sum # повышаем число вагонов
+    если @speed= =0
+      @вагон+=1
+      ставит @wagon
+    конец
+  конец
+  
+  def wagon_del #понижаем число вагонов
+    если @speed= =0
+      @вагон-=1
+      ставит @wagon
+    конец
+  конец
+  
+  def train_route # передаем данные о маршруте
+    @train_r=маршрут.маршрут 0
+    @N=0
+  конец
+  
+  def train_up  #перемещение вперед НЕ УЧИТЫВАЛОСЬ ЧТО ЗНАЧЕНИЕ ПРИВЫСИТ ПОРОГ
+    @N+=1
+    ставит " поезд на станции #{train_r[N]}"
+  конец
+  def train_down #перемещение назад
+    @N- =1
+    ставит " поезд на станции #{train_r[N]}"
+  конец
+ 
